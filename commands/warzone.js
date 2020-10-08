69ef25dbbced2375df3c26cc9937dcbe2d4eaf0f
Nick <ncanino@gmail.com>
2020-10-07 22:54:45 -0400

    Fixed warzone message delay

--- commands/warzone.js
+++ commands/warzone.js
@@ -17,20 +17,8 @@ module.exports = {
         message.channel.send(`Here are some Warzone Stats for ${args[0]}`);
         console.log(`${message.author.tag} sent warzone command at ${Date(Date.now()).toString()}`);            
         API.MWBattleData(args[0], API.platforms.battle).then((output) => {
-            //we'll loop this later
-            //console.log(output);
-            message.channel.send(`Playtime in days: ${output.br_all.timePlayed / 60 / 60 / 24}`);
-            message.channel.send(`Wins: ${output.br_all.wins}`);
-            message.channel.send(`Kills: ${output.br_all.kills}`);
-            message.channel.send(`K/D ratio: ${output.br_all.kdRatio}`);
-            message.channel.send(`Players downed: ${output.br_all.downs}`);
-            message.channel.send(`Deaths: ${output.br_all.deaths}`);
-            message.channel.send(`Teammates revived: ${output.br_all.revives}`);
-            message.channel.send(`Top 25 finishes: ${output.br_all.topTwentyFive}`);
-            message.channel.send(`Top 10 finishes: ${output.br_all.topTen}`);
-            message.channel.send(`Top 5 finishes: ${output.br_all.topFive}`);
-            message.channel.send(`Games played: ${output.br_all.gamesPlayed}`);
-            message.channel.send(`Contracts completed: ${output.br_all.contracts}`)  
+            message.channel.send(`Playtime in days: ${output.br_all.timePlayed / 60 / 60 / 24}\nGames played: ${output.br_all.gamesPlayed}\nWins: ${output.br_all.wins}\nKills: ${output.br_all.kills}\nK/D ratio: ${output.br_all.kdRatio}\nPlayers downed: ${output.br_all.downs}\nDeaths: ${output.br_all.deaths}\nTeammates revived: ${output.br_all.revives}\Top 25 finishes: ${output.br_all.topTwentyFive}\nTop 10 finishes: ${output.br_all.topTen}\nTop 5 finishes: ${output.br_all.topFive}\nContracts completed: ${output.br_all.contracts}`);
+            //console.log(output); 
         })
         .catch((err) => {
             console.error(err);
